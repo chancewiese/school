e5f419322e3a165582e94e22747ab95588ddf836
Chance <chancewiese0925@gmail.com>
2024-08-27 18:24:21 -0600

    more stuff

--- is-6700/Module 2/Module 2 Preparation Assignment 1/assets/scripts/app.js
+++ is-6700/Module 2/Module 2 Preparation Assignment 1/assets/scripts/app.js
@@ -1,3 +1,36 @@
 import { apiKey } from "./util";
 
 console.log(apiKey);
+
+
+
+
+
+let userName = "Test user"
+let userMessage = "Hello World!";
+
+userMessage = "New Value";
+
+console.log(userMessage);
+
+console.log(10 + 5);
+
+// const userMessage = "Hello World!";
+
+function greet(userName, message) {
+    console.log(userName);
+    console.log(message);
+}
+
+function createGreeting(userName, message = "Hello!") {
+    return "Hi, I am " + userName + "." + message;
+}
+
+greet(userName, userMessage);
+greet("Max", "Hello!");
+createGreeting("Max")
+
+export default (userName, message) => {
+    console.log('Hello');
+    return userName + message;
+}
