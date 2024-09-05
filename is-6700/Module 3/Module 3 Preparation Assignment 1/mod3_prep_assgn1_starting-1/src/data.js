0ff9366270798f485ea51fffd90174de948c961b
Chance <chancewiese0925@gmail.com>
2024-09-05 00:21:26 -0600

    mod 3 prep 5

---
+++ is-6700/Module 3/Module 3 Preparation Assignment 1/mod3_prep_assgn1_starting-1/src/data.js
@@ -0,0 +1,28 @@
+import componentsImg from "./assets/logo192.png";
+
+export const CORE_CONCEPTS = [
+    {
+        image: componentsImg,
+        title: 'Components 0',
+        description:
+            'The core UI building block.'
+    },
+    {
+        image: componentsImg,
+        title: 'Components 1',
+        description:
+            'The core UI building block.'
+    },
+    {
+        image: componentsImg,
+        title: 'Components 2',
+        description:
+            'The core UI building block.'
+    },
+    {
+        image: componentsImg,
+        title: 'Components 3',
+        description:
+            'The core UI building block.'
+    },
+]
