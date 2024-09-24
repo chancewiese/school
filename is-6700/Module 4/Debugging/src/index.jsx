73a50eef619ecba4ca9de5103fdc91fcb2461263
Chance <chancewiese0925@gmail.com>
2024-09-24 15:45:02 -0600

    debugging

--- is-6700/Module 4/Debugging/src/index.jsx
+++ is-6700/Module 4/Debugging/src/index.jsx
@@ -1,6 +1,14 @@
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App.jsx';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <App />
+);
+
+
+{/* <StrictMode>
+<App />
+</StrictMode> */}
