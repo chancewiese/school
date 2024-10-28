55ddcf76aee9b8bdf56c97877962bd2be01424dd
Chance <chancewiese0925@gmail.com>
2024-10-27 21:56:21 -0600

    6700 stuff again

--- is-6700/Module-15/mod15-project/src/components/AvailablePlaces.jsx
+++ is-6700/Module-15/mod15-project/src/components/AvailablePlaces.jsx
@@ -1,10 +1,56 @@
-import Places from './Places.jsx';
+import { useState, useEffect } from "react";
+import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    async function fetchPlaces() {
+      setIsFetching(true);
+
+      try {
+        const response = await fetch("http://localhost:3000/places");
+        const resData = await response.json();
+
+        if (!response.ok) {
+          // 200, 300 or 400, 500
+          const error = new Error("Failed to fetch places");
+          throw error;
+        }
+
+        setAvailablePlaces(resData.places);
+      } catch (error) {
+        setError({
+          message:
+            error.message || "Could not fetch places, please try again later.",
+        });
+      }
+
+      setIsFetching(false);
+    }
+
+    fetchPlaces();
+
+    if (error) {
+      return <p>An error occurred: {error.message}</p>;
+    }
+    // fetch("http://localhost:3000/places")
+    //   .then((response) => {
+    //     return response.json();
+    //   })
+    //   .then((resData) => {
+    //     setAvailablePlaces(resData.places);
+    //   });
+  }, []);
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
